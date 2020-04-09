ed4e630efae66ca7b53bd012b0ac2b10a550c6ba
Raphael Marco <raphaelmarco@outlook.com>
2020-04-10 01:03:51 +0800

    Add static server rewrite to index.html

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ const handler = require('serve-handler');
 const distDir = path.join(__dirname, 'dist');
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((request, response) => {
-  return handler(request, response, {
-    public: distDir,
-    directoryListing: false,
-  });
-})
+const server = http.createServer((request, response) => handler(request, response, {
+  public: distDir,
+  directoryListing: false,
+  rewrites: [
+    {
+      source: '/*',
+      destination: 'index.html',
+    },
+  ],
+}));
 
 server.listen(port, () => {
   process.stdout.write(`Running at port ${port}\n`);

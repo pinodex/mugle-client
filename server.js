3fef4dc6b6f88557aeecbda08aa22988abbfbb80
Raphael Marco <raphaelmarco@outlook.com>
2020-04-10 16:34:35 +0800

    Add https redirect to static file server

--- server.js
+++ server.js
@@ -5,16 +5,30 @@ const handler = require('serve-handler');
 const distDir = path.join(__dirname, 'dist');
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((request, response) => handler(request, response, {
-  public: distDir,
-  directoryListing: false,
-  rewrites: [
-    {
-      source: '/*',
-      destination: 'index.html',
-    },
-  ],
-}));
+const isSecure = (request) => request.headers['x-forwarded-proto'] === 'https';
+
+const server = http.createServer((request, response) => {
+  if (!isSecure(request)) {
+    response.writeHead(302, {
+      location: `https://${request.headers.host}${request.url}`,
+    });
+
+    response.end();
+
+    return;
+  }
+
+  handler(request, response, {
+    public: distDir,
+    directoryListing: false,
+    rewrites: [
+      {
+        source: '/*',
+        destination: 'index.html',
+      },
+    ],
+  });
+});
 
 server.listen(port, () => {
   process.stdout.write(`Running at port ${port}\n`);

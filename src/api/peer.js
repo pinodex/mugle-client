cd8b200292f3b01782ed2563ddae140a882cdd85
Raphael Marco <raphaelmarco@outlook.com>
2020-04-11 17:53:38 +0800

    Fix iceServers peerjs config

--- src/api/peer.js
+++ src/api/peer.js
@@ -11,11 +11,25 @@ Object.entries(peerjs).forEach(([key, value]) => {
   }
 });
 
+const prepareIceServers = (iceServers) => iceServers.map((server) => {
+  const newServer = {};
+
+  Object.entries(server).forEach(([key, value]) => {
+    if (value) {
+      newServer[key] = value;
+    }
+  });
+
+  return newServer;
+});
+
 export function createPeerInstance(id, iceServers) {
   instance = new Peer(id, {
     ...config,
     debug: 3,
-    iceServers,
+    config: {
+      iceServers: prepareIceServers(iceServers),
+    },
   });
 
   return instance;
